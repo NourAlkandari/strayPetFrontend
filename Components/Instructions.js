77ddadc2ed1ccc03b0428fc205752cba0f6de8bc
nalrabah <nalrabah@hotmail.com>
2019-04-28 20:44:42 +0300

    added the instructions

--- Components/Instructions.js
+++ Components/Instructions.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import petStore from "../Store/PetStore";
 import * as Animatable from "react-native-animatable";
-import { TouchableWithoutFeedback, TouchableOpacity } from "react-native";
+import {
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 import { Text } from "native-base";
 
 class Instructions extends Component {
@@ -29,27 +33,27 @@ class Instructions extends Component {
             animation="fadeInDown"
             duration="2000"
           >
-            <Text>
-              Congratulations on adopting this lonely stray dog!
-              {petStore.pet.name} needs your care and attention to grow up
-              healthy and happy!
+            <Text style={styles.text}>
+              {`Congratulations on adopting this lonely stray dog! \n ${
+                petStore.pet.name
+              } needs your care and attention to grow up healthy and happy!`}
             </Text>
-            <Text>
+            <Text style={styles.text}>
               It is now your responsibility to make sure your pet meets all
               their needs.
             </Text>
 
-            <Text>
+            <Text style={styles.text}>
               Don't worry though, we'll be giving you hints and tips if you get
               a little confused.
             </Text>
-            <Text>
+            <Text style={styles.text}>
               Becareful though, a pet is a big responsibility and so if you
               leave them for too long, your pet will be sad and it may even get
               ill.
             </Text>
 
-            <Text>
+            <Text style={styles.text}>
               Treat it with love and care, and your pet will love you forever.
             </Text>
           </Animatable.View>
@@ -60,3 +64,14 @@ class Instructions extends Component {
 }
 
 export default Instructions;
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 22,
+    fontFamily: "Noteworthy-Bold",
+    padding: 10,
+    borderRadius: 11,
+    marginLeft: 30,
+    marginRight: 30,
+    minHeight: 80
+  }
+});
